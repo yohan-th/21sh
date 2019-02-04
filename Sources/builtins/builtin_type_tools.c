7b47ac51c776055245a2619d5e3c194a29c522dc
dewalter <deness67@live.fr>
2019-02-04 01:17:04 +0100

    builtin type bug resolved

--- Sources/builtins/builtin_type_tools.c
+++ Sources/builtins/builtin_type_tools.c
@@ -15,29 +15,26 @@
 
 int		builtin_type_get_options(char **options, char **args)
 {
-	int i;
-	int j;
+	int i[2];
 
-	i = 0;
-	while (args[i] && args[i][0] == '-' && args[i][1] && !(j = 0))
-	{
-		if (args[i][1] == '-' && args[i][2] == '\0')
-			break ;
-		while (args[i][++j])
-			if (ft_strchr("afptP", args[i][j]))
+	i[0] = -1;
+	while (args[++i[0]] && args[i[0]][0] == '-' && args[i[0]][1] && !(i[1] = 0))
+		while (args[i[0]][++i[1]])
+			if (!ft_strcmp(args[i[0]], "--") ||
+			ft_strchr("afptP", args[i[0]][i[1]]))
 			{
-				if (!*options || !ft_strchr(*options, args[i][j]))
-					ft_char_join_free(args[i][j], options);
+				if (!ft_strcmp(args[i[0]], "--"))
+					return (i[0] + 1);
+				else if (!*options || !ft_strchr(*options, args[i[0]][i[1]]))
+					ft_char_join_free(args[i[0]][i[1]], options);
 			}
 			else
 			{
 				ft_strdel(options);
-				ft_char_join_free(args[i][j], options);
+				ft_char_join_free(args[i[0]][i[1]], options);
 				return (-1);
 			}
-		i++;
-	}
-	return (i);
+	return (i[0]);
 }
 
 int		builtin_type_check_builtin(char *d_name)
@@ -50,20 +47,22 @@ int		builtin_type_check_builtin(char *d_name)
 	return (0);
 }
 
-void	builtin_type_display(char *d_name, char *bin, char *options, int mode)
+void	builtin_type_display(char *d_name, char *bin, char *op, int mode)
 {
 	if (!mode)
 	{
-		if (options && ft_strchr(options, 't'))
+		if (op && ft_strchr(op, 't') && !ft_strchr(op, 'p')
+		&& !ft_strchr(op, 'P'))
 			ft_putstr("builtin\n");
 		else
 			ft_dprintf(1, "%s is a shell builtin\n", d_name);
 	}
 	else
 	{
-		if (!options || !ft_strchr(options, 't'))
+		if (!op || !ft_strchr(op, 't') || ft_strchr(op, 'P') ||
+		ft_strchr(op, 'p'))
 		{
-			if (!options || !ft_strchr(options, 'P'))
+			if (!op || (!ft_strchr(op, 'P') && !ft_strchr(op, 'p')))
 				ft_dprintf(1, "%s is ", d_name);
 			if (bin)
 				ft_dprintf(1, "%s/%s\n", bin, d_name);
