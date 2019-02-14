7abc0b4dd75b14aefd21ef3fd6eaccbb62b45ebe
yohan-th <ythollet@student.le-101.fr>
2019-02-14 12:33:06 +0100

    plomberie.c

---
+++ Sources/shell/process/shell_process_plomberie.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   shell_process_plomberie.c                        .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: ythollet <ythollet@student.le-101.fr>      +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/02/13 17:46:13 by ythollet     #+#   ##    ##    #+#       */
+/*   Updated: 2019/02/13 17:46:13 by ythollet    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "../../../Include/shell.h"
+
+/*
+** On envoi send_stdin dans le tuyau temporaire tmp_fd.
+** On le ferme pour envoyer EOF
+*/
+
+void	shell_pipe_stdin(int tmp_fd[2], char *send_stdin)
+{
+	ft_putstr_fd(send_stdin, tmp_fd[1]);
+	close(tmp_fd[1]);
+	dup2(tmp_fd[0], 0);
+}
+
+void	shell_pipe_stdout(t_process process)
+{
+	if (process.fd_stdout[0] == '&' && process.fd_stdout[1] != '1')
+		dup2(ft_atoi(process.fd_stdout + 1), 1);
+	else if (process.fd_fileout != 0)
+		dup2(process.fd_fileout, 1);
+}
+
+void	shell_pipe_stderr(t_process process)
+{
+	if (process.fd_stderr[0] == '&' && process.fd_stderr[1] != '2')
+		dup2(ft_atoi(process.fd_stderr + 1), 2);
+	else if (process.fd_fileerr != 0)
+		dup2(process.fd_fileerr, 2);
+}
+
+/*
+** Les redirections stdout sont prio sur le pipe
+*/
+
+void	shell_plomberie(t_cmd *elem, int tmp_fd[2], int fd_pipe[2])
+{
+	if ((elem->process).stdin_send)
+		shell_pipe_stdin(tmp_fd, (elem->process).stdin_send);
+	if (elem->sep == SPL_PIPE && ft_strcmp(elem->process.fd_stdout, "&1") == 0)
+	{
+		dup2(fd_pipe[1], 1);
+		close(fd_pipe[0]);
+		close(fd_pipe[1]);
+	}
+	else
+		shell_pipe_stdout(elem->process);
+	shell_pipe_stderr(elem->process);
+}
